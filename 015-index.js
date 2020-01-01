8b85affc263b507a1b89165de842a653ff976664
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 23:24:22 +0800

    Create 015-index.js

---
+++ 015-index.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b)
+    const size = nums.length
+    const   result = []
+    let l = 0
+    while (l < size) {
+    const n1 = nums[l];
+    let m = l + 1;
+    let r = size - 1;
+    while (m < r) {
+      const n2 = nums[m];
+      const n3 = nums[r];
+      const sum = n1 + n2 + n3;
+      if (sum < 0) {
+        m++;
+        continue;
+      }
+      if (sum > 0) {
+        r--;
+        continue;
+      }
+      result.push([n1, n2, n3]);
+      while (n2 === nums[m]) {
+        m++;
+      }
+      while (n3 === nums[r]) {
+        r--;
+      }
+    }
+        while (n1 ===nums[l]){
+            l++
+        }
+    }
+        return result;
+}
