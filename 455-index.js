c7e4c3f839382380f49a7a230423e844a9215caf
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 21:22:56 +0800

    Create 455-index.js

---
+++ 455-index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    let result = 0;
+    let i = g.length;
+    let j = s.length - 1;
+    g.sort((g1, g2) => g1 - g2)
+    s.sort((s1, s2) => s1 - s2)
+    while (--i >= 0 && j >= 0){
+        const gi =g[i];
+        const sj =s[j]
+        if (sj >= gi){
+            result++
+            j--;
+        }
+    }
+    return result;
+};
