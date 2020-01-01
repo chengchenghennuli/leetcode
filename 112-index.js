989372670c240e1c985341cf99ef18e4577695f2
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 22:40:14 +0800

    Create 112-index.js

---
+++ 112-index.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if (!root){
+        return false
+    }
+    const {left, right, val} = root
+    sum -= val
+    return(!left && !right && sum === 0) || hasPathSum(left, sum) || hasPathSum(right, sum)
+};
