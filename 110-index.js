8e4fdbc2bc825956a339a589ba5a3b4259c34263
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 22:42:05 +0800

    Create 110-index.js

---
+++ 110-index.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    if (!root) {
+    return true;
+  }
+    const left = depth(root.left);
+    const right = depth(root.right);
+    return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+}
+
+     function depth(node) {
+     return !node ? 0 : Math.max(depth(node.left), depth(node.right)) + 1  ;
+};
