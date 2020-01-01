ab4153ab0703147785391628f7ad8457e8c29161
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 23:19:47 +0800

    Create 022-index.js

---
+++ 022-index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    const result = []
+    backtrack('', 0, 0)
+    return result;
+    function backtrack(cur ,left, right){
+        if (cur.length === n * 2){
+            result.push (cur)
+            return;
+        }
+        if (left < n){
+            backtrack(cur + '(', left + 1, right);
+        }
+        if (right < left){
+            backtrack(cur + ')', left, right + 1);
+        }
+    }
+};
