e252a60f977b7a7eb205221ca3c23146d221321f
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 23:21:49 +0800

    Create 019-index.js

---
+++ 019-index.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    let size = 0;
+    let prev = head;
+    let candidate = head;
+    let _head = head;
+    while (_head){
+        _head = _head.next;
+        if (size++ >= n){
+            prev = candidate
+            candidate = candidate.next
+        }
+    }
+    if (head === candidate){
+        return head.next;
+    }
+    prev.next = candidate.next;
+    return head;
+};
