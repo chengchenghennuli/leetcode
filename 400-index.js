47822e88c1d7cfbfb6182594513754ecea27c83e
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 21:33:33 +0800

    Create 400-index.js

---
+++ 400-index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var findNthDigit = function(n) {
+    let sum = 1
+    let digit = 0
+    while(true){
+        const num = 9 * Math.pow(10, digit++) * digit + sum
+        if (n < num){
+            break;
+        }
+        sum = num
+    }
+    n -= sum
+    const pos = n % digit;
+    sum = Math.pow(10, digit - 1) + (n - pos) / digit;
+    sum += '';
+    return +sum[pos];
+};
