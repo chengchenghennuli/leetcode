9ebaf6cafa4f32c760e19d9b1cd8e6f6ebaf00ce
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 23:10:07 +0800

    Create 067-index.js

---
+++ 067-index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a,b) {
+  let la = a.length;
+  let lb = b.length;
+  let prev = 0;
+  let result = '';
+  while (la > 0 || lb > 0) {
+    const na = +a[--la] || 0;
+    const nb = +b[--lb] || 0;
+    const sum = na + nb + prev;
+    result = (sum % 2) + result;
+    prev = (sum / 2) | 0;
+  }
+  if (prev) {
+    result = prev + result;
+  }
+    return result;
+};
