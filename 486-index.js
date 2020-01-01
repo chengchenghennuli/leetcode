ae5d12bdc985c53c944d70af177464a3f5e1f4f2
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 21:08:00 +0800

    Create 486-index.js

---
+++ 486-index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var PredictTheWinner = function(nums) {
+    const l = nums.length;
+    const dp = Array(l);
+    let i = l;
+    while (i--) {
+    dp[i] = Array(l);
+    dp[i][i] = nums[i];
+    for (let j = i + 1; j < l; j++) {
+      dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+    }
+  }
+    return dp[0][l - 1] >= 0;
+
+};
