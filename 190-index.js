7d47fb94b7c9e29135e32f42a380be2fea740db7
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 22:24:07 +0800

    Create 190-index.js

---
+++ 190-index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+    let result = new Uint32Array(1);
+  for (let i = 0; i < 32; i++) {
+    result[0] <<= 1;
+    result[0] |= n & 1;
+    n >>>= 1;
+  }
+  return result[0];
+};
