f3c09d429780f852b4409d99ec724cb770c3e461
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 22:25:02 +0800

    Create 189-index.js

---
+++ 189-index.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    /*const l = nums.length;
+    k = k % l;
+    let count = 0;
+    for (let i = 0; count < l; i++){
+        let current = i;
+        let prev = nums[i];
+        do {
+            const next = (current + k) % l;
+            const temp = nums[next];
+            nums[next] = prev;
+            prev = temp;
+            current = next;
+            count ++;
+        }while (i !== current)
+    } */
+    const l = nums.length;
+    nums.push(...nums.splice(0, l - (k % l)));
+};
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const l = nums.length;
+    k = k % l;
+    let count = 0;
+    for (let i = 0; count < l; i++){
+        let current = i;
+        let prev = nums[i];
+        do {
+            const next = (current + k) % l;
+            const temp = nums[next];
+            nums[next] = prev;
+            prev = temp;
+            current = next;
+            count ++;
+        }while (i !== current)
+    } 
+    
+};
