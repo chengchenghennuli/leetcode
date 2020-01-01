c62acffb7dd164cbde39167e8807b36e36d1dc27
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 22:38:55 +0800

    Create 118-index.js

---
+++ 118-index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    const result = Array(numRows)
+    if (!numRows){
+        return result
+    }
+    for (let i = 0; i < numRows; i++){
+       const array = Array(i + 1)
+       result[i] = array
+       array[0] = 1
+       const prev = result[i - 1]
+       for (let j = 1; j <= i; j++){
+           const n0 = prev[j - 1]
+           const n1 = prev[j] ||0
+           array[j] = n0 + n1
+       }
+    }
+    return result
+};
