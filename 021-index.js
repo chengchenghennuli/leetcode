7ce7b1f79017cbaad763e09203643bc2fc8e8b86
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 23:20:24 +0800

    Create 021-index.js

---
+++ 021-index.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+     const head = { next: null };
+     let h = head;
+     while (l1 && l2) {
+     if (l1.val <= l2.val) {
+      h.next = l1;
+      l1 = l1.next;
+    } 
+      else {
+      h.next = l2;
+      l2 = l2.next;
+    }
+    h = h.next;
+  }
+  h.next = l1 ? l1 : l2;
+  return head.next;
+};
