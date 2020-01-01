809715beed10add080d8b47fb22d9d750cc47030
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-01 21:44:23 +0800

    Create 300-index.js

---
+++ 300-index.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    const l = nums.length;
+    if (l == 0){
+        return 0;
+    }
+    const dp = Array(l).fill(1)
+    for (let i = 0; i < l - 1; i++){
+        const n1 = nums[i]
+        for (let j = i + 1; j < l; j++){
+            const n2 = nums[j]
+            if (n1 >= n2){
+                continue;
+            }
+            dp[j] = Math.max(dp[j], dp[i] + 1)
+        }
+    }
+    return Math.max(...dp)
+};
