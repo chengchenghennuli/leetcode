ae0bf0629975eab0986537e160e15f7c55ee424a
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-02 10:58:33 +0800

    Create 784-index.js

---
+++ 784-index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} S
+ * @return {string[]}
+ */
+var letterCasePermutation = function(S) {
+   S = S.toLowerCase();
+  const l = S.length;
+  return create('', 0);
+  function create(s, i) {
+    if (i === l) {
+      return [s];
+    }
+    const c = S[i++];
+    const code = c.charCodeAt();
+    const result = [];
+    if (code >= 97 & code <= 122) {
+      result.push(...create(s + c, i), ...create(s + c.toUpperCase(), i));
+    } else {
+      result.push(...create(s + c, i));
+    }
+    return result;
+  }
+};
