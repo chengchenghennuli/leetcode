52995202e7bd3ff2c57b6e1070706b1df08c2e92
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-02 10:41:57 +0800

    Create 628-index.js

---
+++ 628-index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    const l = nums.length;
+    if (l < 3){
+        return 0
+    }
+    nums.sort((n1, n2) => (n1 < n2 ? -1 : 1))
+    const max = nums[l - 1]
+    const r1 = max * nums[l - 2] * nums[l - 3];
+    const r2 = max * nums[0] * nums[1];
+    return Math.max(r1, r2)
+};
