b921a447ff8a17c8abc4b7c67cc46519d1b7633d
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-02 11:01:45 +0800

    Create 811-index.js

---
+++ 811-index.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} cpdomains
+ * @return {string[]}
+ */
+var subdomainVisits = function(cpdomains) {
+    const map = new Map();
+    for (const cpdomain of cpdomains) {
+    let [c, domain] = cpdomain.split(' ');
+    const count = +c;
+    while (domain) {
+      const cur = map.get(domain) || 0;
+      map.set(domain, cur + count);
+      const index = domain.indexOf('.');
+      if (index < 0) {
+        break;
+      }
+      domain = domain.slice(index + 1);
+    }
+  }
+  return Array.from(map).map(([domain, count]) => `${count} ${domain}`);
+};
