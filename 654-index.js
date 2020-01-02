6dfc293387c79ba2c27af9f4a07eb9f599a91725
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-02 10:47:58 +0800

    Create 654-index.js

---
+++ 654-index.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var constructMaximumBinaryTree = function(nums) {
+    const l = nums.length;
+     if (!l){
+         return null;
+     }
+     let max = 0;
+     let maxIndex = 0;
+     for (let i = 0; i < l; i++){
+         const n = nums[i];
+         if (n > max){
+             max = n;
+             maxIndex = i;
+         }
+     }
+     const node = new TreeNode(max);
+     node.left = constructMaximumBinaryTree(nums.slice(0, maxIndex)); 
+     node.right = constructMaximumBinaryTree(nums.slice(maxIndex + 1));
+     return node;
+};
