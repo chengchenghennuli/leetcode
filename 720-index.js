d98cf9279edcf1e0eb52bbe563d7ee199c0d699f
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-02 10:53:18 +0800

    Create 720-index.js

---
+++ 720-index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var longestWord = function(words) {
+    words.sort((w1, w2) => w1.localeCompare(w2));
+    const set = new Set([''])
+    let result = ''
+    for (const word of words){
+    if(!set.has(word.slice(0,word.length - 1))){
+        continue;
+    }
+    set.add(word)
+    if (word.length > result.length){
+        result = word;
+    }
+  }
+  return result
+};
