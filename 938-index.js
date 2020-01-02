9d9bb2765ee043074ec9c8c453c98e51a08f85b3
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-02 11:13:29 +0800

    Create 938-index.js

---
+++ 938-index.js
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+var rangeSumBST = function(root, L, R) {
+    /*if (!root){
+        return 0;
+    }
+    const {left, right, val} = root;
+    let sum = val > L ? rangeSumBST(left, L, R) : 0;
+    sum += val >= L && val <= R ? val : 0;
+    return sum + (val < R ? rangeSumBST(right, L, R) : 0)*/  
+    let sum = 0;
+    dfs(root);
+    return sum;
+    function dfs(node) {
+    if (!node) {
+      return;
+    }
+    const { left, right, val } = node;
+    if (val > L) {
+      dfs(left);
+    }
+    if (val >= L && val <= R) {
+      sum += val;
+    }
+    if (val < R) {
+      dfs(right);
+    }
+  }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+var rangeSumBST = function(root, L, R) {
+    if (!root){
+        return 0;
+    }
+    const {left, right, val} = root;
+    let sum = val > L ? rangeSumBST(left, L, R) : 0;
+    sum += val >= L && val <= R ? val : 0;
+    return sum + (val < R ? rangeSumBST(right, L, R) : 0)
+};
