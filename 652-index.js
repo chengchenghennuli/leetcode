4efd114d503bca59ae360da477ba1336b3d5c1da
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-02 10:46:50 +0800

    Create 652-index.js

---
+++ 652-index.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode[]}
+ */
+var findDuplicateSubtrees = function(root) {
+     const map = {};
+     const result = [];
+     resolve(root);
+     return result;
+     function resolve(node){
+         if (!node){
+             return;
+         }
+         const {val, left, right} = node;
+         const key = [val, resolve(left), resolve(right)].join(':')
+         const obj = map[key]
+         if (obj){
+             !obj.result && result.push(obj.node)
+             obj.result = true;
+         }else{
+             map[key] = {node, result:false}
+         }
+         return key
+     }
+};
