e49c1944dcf3dc69a4a627e8810cc46717dcb241
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-02 10:48:29 +0800

    Create 657-index.js

---
+++ 657-index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+     let x = 0;
+     let y = 0;
+     let l = moves.length;
+     while (l--){
+         switch(moves[l]){
+         case 'U':
+             y++;
+             break;
+         case 'D':
+             y--;
+             break;
+         case 'R':
+             x++;
+             break;
+         case 'L':
+             x--;
+             break;
+         }       
+     }
+     return x === 0 & y === 0;
+};
