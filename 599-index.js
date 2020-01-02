c3365d6359b33db9cc03a0b00ca6b920af479d3d
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-02 10:39:55 +0800

    Create 599-index.js

---
+++ 599-index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function(list1, list2) {
+    const map = new Map();
+    for (const [index,item] of list1.entries()){
+        map.set(item, index);
+    }
+    let min = Infinity;
+    const result = [];
+    for (const[index, item] of list2.entries()){
+        if (!map.has(item)){
+            continue;
+        }
+        const sum =map.get(item) + index;
+        if (sum > min){
+            continue;
+        }
+        if (sum < min){
+           min = sum;
+           result.length = 0;
+        }
+        result.push(item);
+    }
+    return result;
+};
