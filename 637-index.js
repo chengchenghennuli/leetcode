0d24dc8b8ca3c563d75e2a04b4efcae7efa65142
chengchenghennuli <50231383+chengchenghennuli@users.noreply.github.com>
2020-01-02 10:43:03 +0800

    Create 637-index.js

---
+++ 637-index.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+    const levels = []
+    dfs(root,0)
+    return levels.map(({sum, count}) => sum / count)
+    function dfs (node,level){
+        if (!node){
+            return
+        }
+        levels[level] = levels[level] || { sum: 0, count: 0 };
+        levels[level].count++
+        levels[level++].sum += node.val;
+        dfs(node.left, level)
+        dfs(node.right, level)
+    }
+};
